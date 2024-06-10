4878b8ebd71710266e1b0f6104bf7967c47a7170
kevin2-cyber <keduful@st.ug.edu.gh>
2024-06-10 13:45:35 +0000

    refactor

--- app/src/main/java/com/example/farm_up/view/FarmerDetailsActivity.java
+++ app/src/main/java/com/example/farm_up/view/FarmerDetailsActivity.java
@@ -26,11 +26,11 @@ public class FarmerDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_farmer_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.farmer_bio), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.farmer_bio), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
         weatherCard = findViewById(R.id.weather_card);
         diseaseCard = findViewById(R.id.disease_card);
